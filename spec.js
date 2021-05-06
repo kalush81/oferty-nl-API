dfe179581be64f7a4237a111d973bad0466ed33c
kalush81 <krzysiek.kaluza@gmail.com>
2021-05-06 15:12:03 +0200

    Add more tests

--- spec.js
+++ spec.js
@@ -1,9 +1,11 @@
 const chai = require('chai').expect;
+const should = require('chai').should();
 const request = require('supertest');
+require('colors')
 
 const app = require('./server');
 
-describe('[RENTALS]', () => {
+describe('[  RENTALS  ]'.yellow, () => {
     it('should get all offers', (done) => {
         request(app)
         .get('/rentals')
@@ -15,5 +17,31 @@ describe('[RENTALS]', () => {
             chai(resp.body[0]).to.have.property('offerId');
             done()
         })
+    })
+    it('should create an offer', (done) => {
+        let offer = {
+            title: 'rower',
+            description: 'mam do wynajcia super rower',
+            daily_price: 5,
+            userId: 2
+        }
+        request(app)
+        .post('/rentals')
+        .send(offer)
+        .set('Accept', 'aplication/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, resp) {
+            const offer = resp.body
+            chai(offer).to.be.an('object')
+            
+            describe('[ rentals ]'.yellow, () => {
+                it('should match regex 5abc', () => {
+                    chai(offer.offerId).to.match(/5abc/)
+                })
+            })
+        })
+        done()
+        
     })
 })
