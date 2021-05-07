94c07e3ac78a6d2abfb897e362262b6d4217a998
kalush81 <krzysiek.kaluza@gmail.com>
2021-05-07 11:41:33 +0200

    Add config file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const app = require('./server/server');
+const config = require('./server/config/config')
 console.log('node env', process.env.MY_VAR)
-app.listen(3000)
+app.listen(config.port, () => {
+    console.log(`server is listening on port: ${config.port}`)
+})
