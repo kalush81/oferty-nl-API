a42eb3b06f4e7f1137c45b68ef6cbec3f3b6ae50
kalush81 <krzysiek.kaluza@gmail.com>
2021-05-09 21:07:39 +0200

    Delete mockupa data (mokup), Rename routes

--- server/api/routes/user/user-model.js
+++ server/api/routes/user/user-model.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    rentals: [mongoose.Schema.Types.ObjectId],
-    services: [mongoose.Schema.Types.ObjectId],
-    isPremium: Boolean,
-})
+  name: String, //must be unique and validated
+  email: String, //must be unique and validated
+  phoneNumber: String, // validate number
+  password: String, // validated
+  rentals: [{ type: mongoose.Schema.Types.ObjectId, ref: "rentals" }], //max 5 rentals, if premium then no max specified
+  services: [{ type: mongoose.Schema.Types.ObjectId, ref: "services" }], //max 5 rentals, if premium then no max specified
+  isPremium: Boolean,
+});
 
-const UserModel = mongoose.model('user', userSchema)
+const UserModel = mongoose.model("user", userSchema);
 
-module.exports = UserModel
+module.exports = UserModel;
